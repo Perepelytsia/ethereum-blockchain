c2e13f767f29b80037a43ff68cb8068bd6171bbe
Perepelytsia <oleksii.perepelytsia@routeique.com>
2025-03-14 23:53:53 -0600

    Add a task

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,6 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+task("receipt", "Transaction Details:")
+  .addParam("hash", "Hash")
+  .setAction(async (taskArgs) => {
+    const receipt = await ethers.provider.getTransactionReceipt(taskArgs.hash);
+
+    if (receipt) {
+        console.log("Transaction Details:", receipt);
+        console.log("Status:", receipt.status === 1 ? "Success" : "Failed");
+    } else {
+        console.log("Transaction not found. It might still be pending.");
+    }
+});
+
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
 };
