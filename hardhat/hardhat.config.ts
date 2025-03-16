57d48019c8532673bd028870cab685f92abecea4
Perepelytsia <oleksii.perepelytsia@routeique.com>
2025-03-15 23:39:25 -0600

    Verified and Published a contract

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -23,6 +23,12 @@ const config: HardhatUserConfig = {
       accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
   },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    enabled: true
+  }
 };
 
 export default config;
